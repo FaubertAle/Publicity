d99b38574b3806e656a95f2fbb7a2aeedc571033
FaubertAle <80648007+FaubertAle@users.noreply.github.com>
2023-05-29 01:11:15 -0500

    Update main.js redes

--- main.js
+++ main.js
@@ -2,28 +2,22 @@ const viewport = document.body.getBoundingClientRect();
 const card = document.querySelector('.card');
 
 const logo = document.querySelector('.logo');
-const spiral = document.querySelector('#spiral');
-const mask = document.querySelector('.mask');
+const background = document.querySelector('#background');
 const photos = document.querySelector('.photos');
-const letters = document.querySelector('.letters');
 
 
 
 if (viewport.height < viewport.width) {
     card.style.width = `${viewport.height*3/4}px`;
-    console.log();
 }
 else{
     card.style.height = `${viewport.width*4/3}px`;
-    console.log('larga');
 }
 
-spiral.style.height = `${card.clientWidth}px`;
+background.style.height = `${card.clientWidth}px`;
 
 logo.addEventListener('click',()=>{
     logo.classList.toggle('active');
-    mask.classList.toggle('active');
-    spiral.classList.toggle('active');
+    background.classList.toggle('active');
     photos.classList.toggle('active');
-    letters.classList.toggle('active');
 })
